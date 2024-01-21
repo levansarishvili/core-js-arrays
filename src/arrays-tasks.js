82ff47fe56d839b5045e27e032fedfaed4da2b93
Levani Sarishvili <levanisarishvili322@gmail.com>
2024-01-21 18:48:43 +0400

    init: start core-js-arrays task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,7 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
 }
 
 /**
